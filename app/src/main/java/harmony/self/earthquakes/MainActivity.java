fa552ec50d2cd776d52530523cb1adddef677371
SelfHarmony <tdnttd@mail.ru>
2016-09-06 16:51:03 +0300

    Добавлен Http запрос на получение json статистики по землетрясениям с сайта USGS.

--- app/src/main/java/harmony/self/earthquakes/MainActivity.java
+++ app/src/main/java/harmony/self/earthquakes/MainActivity.java
@@ -2,6 +2,7 @@ package harmony.self.earthquakes;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -13,34 +14,38 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
+    //CONSTANTS
+    private static String amountOfDataToShow = "50";
+    private static String minmag = "3";
+
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=" +minmag+ "&limit=" + amountOfDataToShow;
+
     public static final String LOG_TAG = MainActivity.class.getName();
+
+    /* Create an {@link EarthQuakeAdapter}, whose data source is a list of
+     {@link earthquakes}s. The adapter knows how to create list item views for each item
+     in the list.*/
+    EarthQuakeAdapter earthQuakeAdapter;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a fake list of earthquakes.
-        ArrayList<EarthQuake> earthquakes = QueryUtils.extractEarthquakes();
 
 
+        new BackgroundNetworking().execute(USGS_REQUEST_URL);
 
-
-        // Find a reference to the {@link ListView} in the layout
-        /*  ListView earthquakeListView = (ListView) findViewById(R.id.list);*/
-
-
-        // Create an {@link EarthQuakeAdapter}, whose data source is a list of
-        // {@link earthquakes}s. The adapter knows how to create list item views for each item
-        // in the list.
-        final EarthQuakeAdapter earthQuakeAdapter = new EarthQuakeAdapter(this, earthquakes);
-
-
-        // Get a reference to the ListView, and attach the adapter to the listView.
         ListView listView = (ListView) findViewById(R.id.list);
+
+        earthQuakeAdapter = new EarthQuakeAdapter(this, new ArrayList<EarthQuake>());
         listView.setAdapter(earthQuakeAdapter);
-
-
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -62,5 +67,33 @@ public class MainActivity extends ActionBarActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
          /*earthquakeListView.setAdapter(adapter);*/
+    }
+
+    private void updateUI(ArrayList<EarthQuake> earthquakes) {
+
+    }
+
+    private class BackgroundNetworking extends AsyncTask<String, Void, ArrayList<EarthQuake>>{
+        @Override
+        protected ArrayList<EarthQuake> doInBackground(String... urls) {
+
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            // Perform the HTTP request for earthquake data and process the response.
+            ArrayList<EarthQuake> earthquakes = QueryUtils.fetchEarthquakeData(urls[0]);
+
+            return earthquakes;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<EarthQuake> result) {
+            earthQuakeAdapter.clear();
+
+                if (result != null && !result.isEmpty()) {
+                    earthQuakeAdapter.addAll(result);
+                }
+
+        }
     }
 }
