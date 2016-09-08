cf52e5e94962a997ff941e44c5759b399aac2498
SelfHarmony <tdnttd@mail.ru>
2016-09-08 23:03:55 +0300

    Добавлена кнопка меню с помощью Fragment. Добавлена options activity.

--- app/src/main/java/harmony/self/earthquakes/MainActivity.java
+++ app/src/main/java/harmony/self/earthquakes/MainActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -145,6 +147,19 @@ public class MainActivity extends ActionBarActivity implements LoaderManager.Loa
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settinsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settinsIntent);
+        }
+    return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/harmony/self/earthquakes/SettingsActivity.java
@@ -0,0 +1,18 @@
+package harmony.self.earthquakes;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+
+public class SettingsActivity extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
